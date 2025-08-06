ecad0b9b5d74c184374f9f500998f7c6b66f3650
crazykarl7 <107001245+crazykarl7@users.noreply.github.com>
2025-08-06 19:12:40 -0400

    Updated UserHome.tsx

--- src/pages/UserHome.tsx
+++ src/pages/UserHome.tsx
@@ -275,6 +275,7 @@ export default function UserHome() {
             {error && (
               <div className="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
                 {error}
+                Game Status
               </div>
             )}
  <button
