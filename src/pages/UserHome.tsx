f57c606204ae019b33ac133f29c2fb769bf520cd
crazykarl7 <107001245+crazykarl7@users.noreply.github.com>
2025-08-08 16:14:08 -0400

    Updated UserHome.tsx

--- src/pages/UserHome.tsx
+++ src/pages/UserHome.tsx
@@ -278,7 +278,7 @@ export default function UserHome() {
               </div>
             )}
  <div>
-  <h2 className="text-lg font-medium text-gray-900 mb-4">Playing Status</h2></div><button
+  <h2 className="text-lg font-medium text-gray-900 mb-4">Playing Status</h2><button
                   onClick={handleTogglePresence}
                   disabled={updatingPresence}
                   className={clsx(
@@ -300,7 +300,7 @@ export default function UserHome() {
                       Pause
                     </>
                   )}
-                </button>
+                </button> </div>
             {/* Available Olympics */}
             {availableOlympics.length > 0 && (
               <div className="mb-8">
