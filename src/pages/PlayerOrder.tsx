f9fa6bab5df5531a8cd7e7a0a2b0bfd097eb3766
crazykarl7 <107001245+crazykarl7@users.noreply.github.com>
2025-08-07 21:48:30 -0400

    Updated PlayerOrder.tsx

--- src/pages/PlayerOrder.tsx
+++ src/pages/PlayerOrder.tsx
@@ -14,6 +14,7 @@ import {
   Clock
 } from 'lucide-react';
 import clsx from 'clsx';
+import CustomAvatar from '../components/CustomAvatar';
 
 interface PlayerWithPresence extends Profile {
   is_present: boolean;
