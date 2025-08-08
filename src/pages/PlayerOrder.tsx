574d83f08dd7c596b4e5718691a8bf223cc6d5e7
crazykarl7 <107001245+crazykarl7@users.noreply.github.com>
2025-08-07 21:51:08 -0400

    Updated PlayerOrder.tsx

--- src/pages/PlayerOrder.tsx
+++ src/pages/PlayerOrder.tsx
@@ -407,12 +407,14 @@ export default function PlayerOrder() {
                                     <Users className="h-5 w-5 text-indigo-600" />
                                   </div>
                                 )} */}
-                                <CustomAvatar
+                                <div className="h-10 w-10 flex-shrink-0">
+                                    <CustomAvatar
                                       shape={score.player?.avatar_shape}
                                       foregroundColor={score.player?.avatar_foreground_color}
                                       backgroundColor={score.player?.avatar_background_color}
                                       size="medium"
                                     />
+                                  </div>
                               </div>
                               <div className="ml-4">
                                 <div className="font-medium text-gray-900">
